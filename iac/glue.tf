99ae15650fa41e138ababcfe23a0a9a01a5912de
shiva1312 <108486744+shiva1312@users.noreply.github.com>
2025-12-23 17:01:59 +0300

    Update glue.tf

--- iac/glue.tf
+++ iac/glue.tf
@@ -1,1 +1,23 @@
+resource "aws_glue_job" "etl" {
+  name     = "clickstream-etl"
+  role_arn = aws_iam_role.glue_role.arn
+
+  command {
+    name            = "glueetl"
+    script_location = "s3://${aws_s3_bucket.glue_scripts.bucket}/transform_json_to_parquet.py"
+    python_version  = "3"
+  }
+
+  glue_version = "4.0"
+}
+
+resource "aws_glue_crawler" "crawler" {
+  name          = "clickstream-crawler"
+  role          = aws_iam_role.glue_role.arn
+  database_name = "clickstream_db"
+
+  s3_target {
+    path = "s3://${aws_s3_bucket.processed.bucket}"
+  }
+}
 
