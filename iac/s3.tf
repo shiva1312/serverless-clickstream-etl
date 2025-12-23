9c6752cfc0753116e04d190e14e89b8535983e88
shiva1312 <108486744+shiva1312@users.noreply.github.com>
2025-12-23 16:58:53 +0300

    Update s3.tf

--- iac/s3.tf
+++ iac/s3.tf
@@ -1,1 +1,24 @@
+  GNU nano 8.3                                                                            s3.tf                                                                                       
+resource "random_id" "suffix" {
+  byte_length = 4
+}
+
+resource "aws_s3_bucket" "raw" {
+  bucket = "clickstream-raw-${random_id.suffix.hex}"
+
+  tags = {
+    Name        = "ClickstreamRaw"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket" "processed" {
+  bucket = "clickstream-processed-${random_id.suffix.hex}"
+
+  tags = {
+    Name        = "ClickstreamProcessed"
+    Environment = "Dev"
+  }
+}
+
 
