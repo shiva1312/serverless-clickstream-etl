52b642eaac42106d66eca17b35427c69ea465c18
shiva1312 <108486744+shiva1312@users.noreply.github.com>
2025-12-23 17:00:26 +0300

    Update iam.tf

--- iac/iam.tf
+++ iac/iam.tf
@@ -1,1 +1,34 @@
+resource "aws_iam_role" "glue_role" {
+  name = "glue-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = { Service = "glue.amazonaws.com" }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_policy" "glue_policy" {
+  name = "GlueCustomPolicy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Action = [
+        "s3:*",
+        "logs:*"
+      ]
+      Resource = "*"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "glue_attach" {
+  role       = aws_iam_role.glue_role.name
+  policy_arn = aws_iam_policy.glue_policy.arn
+}
 
