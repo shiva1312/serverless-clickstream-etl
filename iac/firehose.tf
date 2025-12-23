60faf1cf16a517da4962f0caa0e2192fab6f7309
shiva1312 <108486744+shiva1312@users.noreply.github.com>
2025-12-23 17:01:14 +0300

    Update firehose.tf

--- iac/firehose.tf
+++ iac/firehose.tf
@@ -1,1 +1,28 @@
+resource "aws_iam_role" "firehose_role" {
+  name = "firehose-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = { Service = "firehose.amazonaws.com" }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "firehose_policy" {
+  role       = aws_iam_role.firehose_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_kinesis_firehose_delivery_stream" "clickstream" {
+  name        = "clickstream-firehose"
+  destination = "extended_s3"
+
+  extended_s3_configuration {
+    role_arn   = aws_iam_role.firehose_role.arn
+    bucket_arn = aws_s3_bucket.raw.arn
+  }
+}
 
