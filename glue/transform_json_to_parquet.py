e823719ceb5efbd2a23abf2090cdfcacec639d56
shiva1312 <108486744+shiva1312@users.noreply.github.com>
2025-12-23 17:07:21 +0300

    Update transform_json_to_parquet.py

--- glue/transform_json_to_parquet.py
+++ glue/transform_json_to_parquet.py
@@ -1,1 +1,27 @@
+from awsglue.context import GlueContext
+from pyspark.context import SparkContext
+from awsglue.transforms import *
+
+sc = SparkContext()
+glueContext = GlueContext(sc)
+
+
+df = glueContext.create_dynamic_frame.from_options(
+    connection_type="s3",
+    format="json",
+    connection_options={"paths": ["s3://clickstream-raw-c2a7f5b3/"]}
+)
+
+cleaned = DropFields.apply(
+    frame=df,
+    paths=["ip_address", "user_agent"]
+)
+
+
+glueContext.write_dynamic_frame.from_options(
+    frame=cleaned,
+    connection_type="s3",
+    format="parquet",
+    connection_options={"path": "s3://clickstream-processed-c2a7f5b3/"}
+)
 
