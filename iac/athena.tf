49364a0b98697926e89230f9fcd35a959b6c4711
shiva1312 <108486744+shiva1312@users.noreply.github.com>
2025-12-25 16:33:08 +0300

    Update athena.tf

--- iac/athena.tf
+++ iac/athena.tf
@@ -1,6 +1,16 @@
+########################################
+# Amazon Athena Database
+########################################
+
 resource "aws_athena_database" "db" {
-  name   = "clickstream_db"
+  name = "clickstream_db"
+
   bucket = aws_s3_bucket.processed.bucket
+
+  # Dedicated prefix for Athena metadata
+  properties = {
+    location = "s3://${aws_s3_bucket.processed.bucket}/athena/"
+  }
+
+  force_destroy = true
 }
-
-
